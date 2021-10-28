0823bd3af51520f0fc87a30dc7cb3a877bf13030
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-28 20:56:00 +0530

    Created a view for the list of recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def recipes(request):
+    api = [{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg'}]
+    return render(request,'index.html',name="home")
