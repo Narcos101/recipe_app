35f9b342085c0ad03493e315466126d1a302dc0f
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-28 23:05:25 +0530

    Adding a dynamic path segment to our details page

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-from .views import recipes
+from .views import recipes,recipe_details
 urlpatterns = [
-    path('recipes/',recipes,name="home")
+    path('recipes/',recipes,name="home"),
+    path('recipes/<recipe_id>',recipe_details)
 ]
