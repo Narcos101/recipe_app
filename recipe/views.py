31419981d9b256568d228ac5eea4553959948707
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-28 23:02:05 +0530

    Created a view for recipe details

--- recipe/views.py
+++ recipe/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def recipes(request):
-    api = [{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg'},{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg'}]
+    api = [{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg'},{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg','timetaken':'50minutes'}]
     return render(request,'index.html',{'api':api})
+
+
+def recipe_details(request):
+    return render(request,'recipe_details.html')
