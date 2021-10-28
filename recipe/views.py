87158c37a359e105c539a27367b07e93a6044a8e
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-28 23:58:27 +0530

    Adding a special id

--- recipe/views.py
+++ recipe/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def recipes(request):
-    api = [{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg'},{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg','timetaken':'50minutes'}]
+    api = [{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg','id':'1'},{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg','timetaken':'50minutes','id':'2'}]
     return render(request,'index.html',{'api':api})
 
 
-def recipe_details(request):
+def recipe_details(request,recipe_id):
     return render(request,'recipe_details.html')
