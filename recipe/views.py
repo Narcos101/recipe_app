84e35c4ddd37372b2be6cfe6c25a30fdf697b5ad
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-28 20:58:00 +0530

    removed useless info

--- recipe/views.py
+++ recipe/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def recipes(request):
     api = [{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg'}]
-    return render(request,'index.html',name="home")
+    return render(request,'index.html')
