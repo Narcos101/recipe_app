76ee73f4d776d6a0e401312aed5309f80013c661
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-30 14:28:46 +0530

    added a url for the recipe add page

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from .views import recipes,recipe_details
+from .views import recipes,recipe_details,recipe_add
 urlpatterns = [
     path('recipes/',recipes,name="home"),
-    path('recipes/<recipe_id>',recipe_details)
+    path('recipes/<recipe_id>',recipe_details),
+    path('recipe',recipe_add,name="add")
 ]
