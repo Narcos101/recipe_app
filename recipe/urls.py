c29ace33d567297bc0d7ba21513cf1bf99fa3cb2
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-30 18:41:12 +0530

    Url for adding recipes

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from .views import recipes,recipe_details,recipe_add
 urlpatterns = [
     path('recipes/',recipes,name="home"),
+    path('recipes/add',recipe_add,name="add"),
     path('recipes/<recipe_id>',recipe_details),
-    path('recipe',recipe_add,name="add"),
 ]
