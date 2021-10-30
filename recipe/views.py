2991d2299a5942ddd29139a795147fdf10d15da2
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-30 10:08:11 +0530

    taking input data from our models

--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import recipe
 
 # Create your views here.
 def recipes(request):
-    api = [{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg','id':'1'},{'title':'Chicken kabab','description':'lorem ipsum','image':'https://img-global.cpcdn.com/recipes/53501194d626e345/400x400cq70/photo.jpg','timetaken':'50minutes','id':'2'}]
+    api = recipe.objects.all()
     return render(request,'index.html',{'api':api})
 
 
