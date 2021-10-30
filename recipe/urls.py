2cc007af199abde3ff2e226e09e776e1c1d7b8e7
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-30 16:09:09 +0530

    Making a url for success page when recipe is added successfully

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,5 +3,6 @@ from .views import recipes,recipe_details,recipe_add
 urlpatterns = [
     path('recipes/',recipes,name="home"),
     path('recipes/<recipe_id>',recipe_details),
-    path('recipe',recipe_add,name="add")
+    path('recipe',recipe_add,name="add"),
+    path('success',success,name="success")
 ]
