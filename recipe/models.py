57cee23144b982603322615693e92b78a92f9cd2
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-30 18:40:56 +0530

    Upload all the images to images subdir

--- recipe/models.py
+++ recipe/models.py
@@ -4,7 +4,7 @@ class recipe(models.Model):
     title=models.CharField(max_length=200)
     description=models.TextField()
     timetaken=models.IntegerField()
-    image=models.ImageField(upload_to='images')
+    image=models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title + "-" + str(self.timetaken)
