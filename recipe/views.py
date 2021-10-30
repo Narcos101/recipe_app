ef73cd69eee716b273a51c528c5801a20bd2611c
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-30 12:20:00 +0530

    Error handling for recipe not found\

--- recipe/views.py
+++ recipe/views.py
@@ -8,4 +8,8 @@ def recipes(request):
 
 
 def recipe_details(request,recipe_id):
-    return render(request,'recipe_details.html')
+    try:
+        selected_recipe = recipe.objects.get(id=recipe_id)
+        return render(request,'recipe_details.html',{'selected_recipe':selected_recipe})
+    except:
+        return render(request,'404.html')
