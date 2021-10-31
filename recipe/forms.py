6c457e8e0e66aa4da9320c239f31f4d5cda1cf44
Narcotics101 <vivekjaiswar899@gmail.com>
2021-10-31 13:23:06 +0530

    Use of django widgets to make the form look more professional

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 
+from django.forms import widgets
+
 from .models import recipe
 
 class Recipeform(forms.ModelForm):
     class Meta:
         model = recipe
-        fields = '__all__'
+        fields = "__all__"
+        widgets={
+            'title': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter the name of your Recipe'}),
+            'description': forms.Textarea(attrs={'class':'form-control','placeholder':'Description of your recipes and steps on how to prepare it'}),
+            'timetaken':forms.NumberInput(attrs={'class':'form-control','placeholder':'Enter the time taken (in minutes) to prepare your recipe'}),
+            'image':forms.FileInput(attrs={'class':'form-control','placeholder':'The image of your recipe'})
+        }
